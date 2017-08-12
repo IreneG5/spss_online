18dca0ba69ab102eae01a03709e532790f3a514d
IreneG5 <irene.g5555@gmail.com>
2017-08-12 19:30:01 +0100

    Add basic home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+
 # Create your views here.
+def get_index(request):
+    return render(request, 'index.html')
--- spss_online/urls.py
+++ spss_online/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from home import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.get_index)
 ]
