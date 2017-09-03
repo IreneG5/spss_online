cf93d5c86b0386da42f1b154449d6d54b4363698
IreneG5 <irene.g5555@gmail.com>
2017-09-03 23:19:04 +0100

    Update blog - responsive boxes & add images

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list),
-    url(r'^blog/(?P<id>\d+)/$', views.post_detail),
+    url(r'^blog/$', views.post_list, name='blog'),
+    url(r'^blog/(?P<id>\d+)/$', views.post_detail, name='post-detail'),
 ]
