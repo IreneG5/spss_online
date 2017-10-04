0c47ccd973b1792e8464966c695408b1ef7d4ba4
IreneG5 <irene.g5555@gmail.com>
2017-10-04 20:07:20 +0100

    Config DB in Heroku

--- tickets/migrations/0002_auto_20170917_0125.py
+++ tickets/migrations/0002_auto_20170917_0125.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='ticket',
             name='status',
-            field=models.ForeignKey(default='New', on_delete=django.db.models.deletion.CASCADE, related_name='tickets', to='tickets.Status'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='tickets', to='tickets.Status'),
         ),
     ]
