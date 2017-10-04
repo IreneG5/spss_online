4bf66d7063579d18f6b34d6a95f77b5d3b004587
IreneG5 <irene.g5555@gmail.com>
2017-10-04 21:38:24 +0100

    Config DB in Heroku

--- settings/base.py
+++ settings/base.py
@@ -157,8 +157,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_STORAGE_BUCKET_NAME = 'spssonlinebucket'
 AWS_S3_REGION_NAME = 'eu-west-1'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAIJC3L57P63EWVSQA'
-AWS_SECRET_ACCESS_KEY = 'yrvWSmclI2cZCJbs9U0TFp0XzTTk4YEn9EMVecYC'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
