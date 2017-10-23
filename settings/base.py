0d56faf726f019c7d763a7c683246761883dbc1d
IreneG5 <irene.g5555@gmail.com>
2017-10-23 20:01:44 +0100

    Minor fixes

--- settings/base.py
+++ settings/base.py
@@ -118,7 +118,7 @@ AUTHENTICATION_BACKENDS = (
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
