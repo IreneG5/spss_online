6a6338d9cac51a20a5e66ce4a430f34a191eb133
IreneG5 <irene.g5555@gmail.com>
2017-10-23 19:53:58 +0100

    Minor fixes

--- paypal_store/views.py
+++ paypal_store/views.py
@@ -18,25 +18,28 @@ register = template.Library()
 def paypal_return(request):
     """
     Render paypal_return.html template when the purchase through paypal finish successfully (on PayPal side).
-    Retrieve the last purchase for the user (checking it was made in the last 3 minutes) and show details
-    on the screen.
+    Every second retrieve the last purchase and compare it with the current date to
+    see if it was made in the last 3 minutes (enough time for paypal to send the signal
+    and the user to come back to the merchant's page.
+    If a last purchase is found and it was in the last 3 minutes, it renders the page
+    with the relevant information for the user, otherwise it shows a message.
     """
+
     if User.is_authenticated:
         user = auth.get_user(request)
-        last_purchase = Purchase.objects.filter(user_id=user).last()
-        year = last_purchase.license_end.year - int(last_purchase.product.license_type[0])
-        last_purchase_date = last_purchase.license_end.replace(year=year)
-
         # Check every 3 seconds (up to 30 seconds) if the purchase was saved in case the PayPal signal is delayed
         not_found = 0
-        while not_found < 10:
-            if last_purchase_date < arrow.utcnow().replace(seconds=-180).datetime:
-                not_found += 1
-                time.sleep(3)
-            else:
-                break
+        while not_found < 30:
+            last_purchase = Purchase.objects.filter(user_id=user).last()
+            if last_purchase:
+                year = last_purchase.license_end.year - int(last_purchase.product.license_type[0])
+                last_purchase_date = last_purchase.license_end.replace(year=year)
+                if last_purchase_date >= arrow.utcnow().replace(seconds=-180).datetime:
+                    break
+            not_found += 1
+            time.sleep(1)
 
-        if not_found == 10:
+        if not_found == 30:
             messages.error(request, "There was a problem retrieving your purchase.",
                            extra_tags='alert alert-danger')
             args = {'html': "Please refresh the page. "
